5d9111be8466aff7f6a7df5c41dbdaaa39820fca
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-03 02:53:05 -0600

    > Se agregan modelos de la base de datos > Un Steamer tiene muchos videos, es una relacion de uno a muchos

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchitecture.Domain
+{
+    public class Streamer
+    {
+        [Key]
+        public int Id           { get; set; }
+        public string? Name     { get; set; }
+        public string? Url      { get; set; }
+    }
+}
---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public long Id          { get; set; }
+        public string? Name     { get; set; }
+        public int StreamerId   { get; set; }
+
+        /// <summary>
+        /// Clase que representa a la identidad
+        /// Indicarle este puede ser sobreesicrta 
+        /// por una clase derivada
+        /// </summary>
+        public virtual Streamer? Streamer { get; set; }
+    }
+}
