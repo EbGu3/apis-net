dd7328aadf8587894718df5094e0317cb25855a4
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-03 20:06:57 -0600

    > Probando funciones de entity framework con EF.Function > Comparando resultados y consulta con Equals vs "==" > Probando consultas con Contains, para que el valor pueda parecerse a algun registros simula funcion LIKE

--- src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
+++ src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -1,10 +1,51 @@
 ﻿using CleanArchitecture.Data;
 using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext dbContext = new();
 
-await AddNewRecords();
-QueryStreaming();
+await QueryFilterPartialResult();
+
+async Task QueryFilterPartialResult () 
+    => await dbContext!.Streamers!
+              .Where(record =>
+                  EF.Functions.Like(record!.Name, $"%Rubi%")
+              )
+              .ForEachAsync(record =>
+              {
+                  Console.WriteLine($"Streamer: {record.Name}");
+              });
+
+
+async Task QueryFilterContains ()
+    => await dbContext!.Streamers!
+              .Where(record =>
+                record!.Name!.Contains("Rubi")
+              )
+              .ForEachAsync(record =>
+              {
+                  Console.WriteLine($"Streamer: {record.Name}");
+              });
+
+async Task QueryFilterEquals ()
+    => await dbContext!.Streamers!
+              .Where(record =>
+                record!.Name!.Equals("Rubiely")
+              )
+              .ForEachAsync(record =>
+              {
+                  Console.WriteLine($"Streamer: {record.Name}");
+              });
+
+async Task QueryFilter ()
+    => await dbContext!.Streamers!
+              .Where(record => 
+                record.Name == "Rubiely"
+              )
+              .ForEachAsync(record =>
+              {
+                  Console.WriteLine($"Streamer: {record.Name}");
+              });
 
 void QueryStreaming ()
     => dbContext!.Streamers!
