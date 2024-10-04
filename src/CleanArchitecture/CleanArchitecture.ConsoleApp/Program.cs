9e34432112a1a89c08ffa0f065e286a833511197
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-03 21:48:26 -0600

    > Diferentes casos de uso de funciones de Entity

--- src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
+++ src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -4,7 +4,55 @@ using Microsoft.EntityFrameworkCore;
 
 StreamerDbContext dbContext = new();
 
-await QueryFilterPartialResult();
+//await QueryFilterPartialResult();
+await QueryMethods();
+
+async Task QueryMethods()
+{
+    DbSet<Streamer> streamers = dbContext!.Streamers!;
+
+    // No usar '' da error mejor usar "", ya que da error el primero
+
+    // Devuelve el primer registro
+    // Si no lo encuentra tira una excepcion
+    Streamer firstAsync = await streamers
+                                    .Where(record =>
+                                        record!.Name!.Contains("I")
+                                    )
+                                    .FirstAsync();
+
+    // Devuelve el primer registro que cumple con la condición
+    // o devuelve null
+    Streamer? firstOrDeafulAsync = await streamers
+                                            .Where(record =>
+                                                record!.Name!.Contains("I")
+                                            )
+                                            .FirstOrDefaultAsync();
+
+    // Devuelve el primer registro que cumpla con la condicion
+    // o devuelve null
+    Streamer? firstOrDefaultV2 = await streamers
+                                        .FirstOrDefaultAsync(record =>
+                                            record!.Name!.Contains("I")
+                                        );
+
+    // SingleAsync vs FirstOrDefaultAsync
+    // SingleAsync, el resultado de la condicion aplicada solo puede devolver un valor, si detecta una coleccion tira error
+    // Si no encuentra ningun registro que cumpla con la condicion tira error
+    var singleAsync = await streamers
+                            .Where(record => record.Id == 1)
+                            .SingleAsync();
+
+    // Devuelve el primer registro que cumple con la condición
+    // no tira excepcion
+    var singleOrDefaultAsync = await streamers
+                                        .Where(record => record.Id == 1)
+                                        .SingleOrDefaultAsync();
+
+    // Busqueda por id
+    var findAsync = streamers.FindAsync(1);
+
+}
 
 async Task QueryFilterPartialResult () 
     => await dbContext!.Streamers!
@@ -15,7 +63,6 @@ async Task QueryFilterPartialResult ()
               {
                   Console.WriteLine($"Streamer: {record.Name}");
               });
-
 
 async Task QueryFilterContains ()
     => await dbContext!.Streamers!
