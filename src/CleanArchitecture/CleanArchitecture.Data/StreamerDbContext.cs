737dc896567c56cd57d871bfdedad8e7919b14c8
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-03 19:24:37 -0600

    > Se agrega la primera migracion "add-migration MigracionInicial" > Se agrega a la base de datos "update-database" > Se agregan paquetes Desing en ConsoleApp > Se agrega EntityFramework.SqlServer en .Data > Se agrega EntityFramework.Tools en .Data > Se agregan referencias a proyectos > Se agrega el StreamerDbContext

---
+++ src/CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Server=localhost;Database=Streamer;User Id=sa;Password=eguerra;TrustServerCertificate=True");
+        }
+
+        public DbSet<Streamer>? Streamers    { get; set; }
+        public DbSet<Video>? Videos          { get; set; }
+    }
+}
