e8b7aeb549074c1e680db778ae0e15ae12e823eb
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-03 19:47:55 -0600

    Refactorianzado consultas e inserciones

--- src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
+++ src/CleanArchitecture/CleanArchitecture.ConsoleApp/Program.cs
@@ -3,35 +3,50 @@ using CleanArchitecture.Domain;
 
 StreamerDbContext dbContext = new();
 
-// Insertar solo una
-Streamer streamer = new()
+await AddNewRecords();
+QueryStreaming();
+
+void QueryStreaming ()
+    => dbContext!.Streamers!
+              .ToList()
+              .ForEach(record =>
+              {
+                  Console.WriteLine($"Streamer: {record.Name}");
+              });
+
+async Task AddNewRecords ()
 {
-    Name = "Ivanna",
-    Url = "https://www.twitch.tv/IR"
-};
-
-//! me indica que esta instanciado
-dbContext!.Streamers!.Add(streamer);
-await dbContext!.SaveChangesAsync();
-
-// Insertar varias peliculas
-List<Video> videos =
-[
-    new Video
+    // Insertar solo una
+    Streamer streamer = new()
     {
-        Name = "Mi love",
+        Name = "Rubiely",
+        Url = "https://www.twitch.tv/IR"
+    };
+
+    //! me indica que esta instanciado
+    dbContext!.Streamers!.Add(streamer);
+    await dbContext!.SaveChangesAsync();
+
+    // Insertar varias peliculas
+    List<Video> videos =
+    [
+        new Video
+    {
+        Name = "Beautiful Princess",
         StreamerId = streamer.Id
     },
     new Video
     {
-        Name = "Mi beautiful girl",
+        Name = "The love history",
         StreamerId = streamer.Id
     },
     new Video {
-        Name = "Mi life con Ella <3",
+        Name = "The best Girl",
         StreamerId = streamer.Id
     }
-];
+    ];
 
-await dbContext!.AddRangeAsync(videos);
-await dbContext!.SaveChangesAsync();
+    await dbContext!.AddRangeAsync(videos);
+    await dbContext!.SaveChangesAsync();
+
+}
