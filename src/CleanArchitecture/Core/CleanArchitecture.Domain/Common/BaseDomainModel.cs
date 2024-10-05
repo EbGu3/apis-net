f1d07c81fd5244a9dd5e9962a0d82f1ba3344122
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-05 11:11:23 -0600

    Refactorización y nuevas funcionalidades
    - Se eliminó la clase `Class1` del archivo `Class1.cs`.
    - Se añadieron nuevas carpetas `Features` y `Behaviours` en el archivo `CleanArchitecture.Application.csproj`.
    - En la clase `BaseDomainModel` del archivo `BaseDomainModel.cs`, se añadieron nuevas propiedades: `CreateDate`, `CreatedBy`, `LastModifiedDate` y `LastModifiedBy`.
    - La clase `VideoActor` en el archivo `VideoActor.cs` ahora hereda de `BaseDomainModel`.
    - Se creó una nueva interfaz `IAsyncRepository` en el archivo `IAsyncRepository.cs` con métodos para operaciones asíncronas de CRUD.
    - Se añadió una nueva clase abstracta `ValueObject` en el archivo `ValueObject.cs` para representar objetos de valor inmutables con métodos de comparación y hash.

--- src/CleanArchitecture/Core/CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ src/CleanArchitecture/Core/CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -2,6 +2,10 @@
 {
     public class BaseDomainModel
     {
-        public int Id { get; set; }
+        public int Id                       { get; set; }
+        public DateTime? CreateDate         { get; set; }
+        public string? CreatedBy            { get; set; }
+        public DateTime? LastModifiedDate   { get; set; }
+        public string? LastModifiedBy       { get; set; }
     }
 }
