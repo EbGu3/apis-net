b23717376990d4c3de2dee27288c47f7df7a34d8
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-05 12:59:35 -0600

    Añadir soporte para comandos de Streamer
    Se ha añadido una nueva carpeta `Features\Streamers\Queries` en el archivo `CleanArchitecture.Application.csproj`.
    
    Se ha creado una nueva interfaz `IStreamerRepository` en el archivo `IStreamerRepository.cs`, que extiende `IAsyncRepository<Streamer>`.
    
    Se ha añadido una nueva clase `StreamerCommand` en el archivo `StreamerCommand.cs`, que implementa `IRequest<int>` y contiene las propiedades `Name` y `Url`.
    
    Se ha añadido una nueva clase `StreamerCommandHandler` en el archivo `StreamerCommandHandler.cs`, que implementa `IRequestHandler<StreamerCommand, int>`. Esta clase maneja la lógica para agregar un nuevo `Streamer` y enviar un correo electrónico de notificación.

---
+++ src/CleanArchitecture/Core/CleanArchitecture.Application/Features/Streamers/Commands/StreamerCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace CleanArchitecture.Application.Features.Streamers.Commands
+{
+    public class StreamerCommand : IRequest<int>
+    {
+        public required string Name { get; set; }
+        public string Url           { get; set; } = string.Empty;
+    }
+}
