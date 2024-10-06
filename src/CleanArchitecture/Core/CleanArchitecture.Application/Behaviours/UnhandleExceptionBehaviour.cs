c1e8e552e8b5e395ea61077cf11a0a1c13295333
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2024-10-05 18:14:01 -0600

    Refactorización y nuevas clases de comportamiento
    Se agregan las siguientes clases en Behaviours para detectar errores antes de llenar al proceso
    
    Se ha añadido la clase `UnhandleExceptionBehaviour` en `UnhandleExceptionBehaviour.cs`, que implementa `IPipelineBehavior` de MediatR para manejar excepciones no controladas y registrar errores con `ILogger`.
    
    Se ha añadido la clase `ValidationBehaviour` en `ValidationBehaviour.cs`, que también implementa `IPipelineBehavior` de MediatR para validar solicitudes con `FluentValidation` y lanzar excepciones `ValidationException` en caso de fallos de validación.

---
+++ src/CleanArchitecture/Core/CleanArchitecture.Application/Behaviours/UnhandleExceptionBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.Behaviours
+{
+    public class UnhandleExceptionBehaviour<TRequest, TResponse> 
+        (
+            ILogger<TRequest> logger
+        )
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger = logger;
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Sucedio una excepcion para el request {} {}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
